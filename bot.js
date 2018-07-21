00bb982844057d026a84c412f7106519af55da11
twmgamer <41520074+twmgamer@users.noreply.github.com>
2018-07-21 14:12:49 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+
+const TOKEN = "NDY4NTAwMzY3OTg2OTE3Mzg2.DjMHkQ.ezHdPnmUWm3ZAXobJ_qpg53vIAY"
+const PREFIX = ".";
+
+var bot = new Discord.Client();
+
+bot.on("Ready", function() {
+    console.log("Ready");
+    bot.user.setActivity("New Users", {type: 2});
+});
+
+client.login(process.env.BOT_TOKEN);
